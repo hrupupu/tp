08debb9ba59a4818f3bdf4fc76ab20df4e1563bd
Adelya Khamitova <adi.khamitova@gmail.com>
2022-07-22 17:20:37 +0300

    3

--- smth.py
+++ smth.py
@@ -1,13 +1,11 @@
 import random
 import string
-def ff():
-    def line():
-        global res
+def line():
         n = random.randint(256, 512)
-        res = ''.join(random.choices(string.ascii_letters + string.digits, k = n)) + '\n'
+        return ''.join(random.choices(string.ascii_letters + string.digits, k = n)) + '\n'
+def ff():
     file = "test.txt"
     with open (file, 'w') as f:
         for i in range(random.randint(10000, 20000)):
-            line()
-            f.write(res)
+            f.write(line())
 ff()
